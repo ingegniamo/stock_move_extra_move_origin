ef85a57faac17a648b27d75dbec180fdb4d9e1aa
Michele <dicroce.m@stesi.eu>
2023-10-02 11:58:50 +0200

    first draft

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,18 +1,6 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
-from collections import defaultdict
-from datetime import timedelta
-from operator import itemgetter
 
 from odoo import _, api, Command, fields, models
-from odoo.exceptions import UserError
-from odoo.osv import expression
-from odoo.tools.float_utils import float_compare, float_is_zero, float_round
-from odoo.tools.misc import clean_context, OrderedSet, groupby
-
-PROCUREMENT_PRIORITIES = [('0', 'Normal'), ('1', 'Urgent')]
 
 
 class StockMove(models.Model):
